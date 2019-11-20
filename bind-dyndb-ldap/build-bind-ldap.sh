2a33bca039c9880bbb82bff1ce6ba76fa3c36729
root <root@b457f2d1e3ac>
2019-11-20 21:35:46 +0000

    Update bind9-dyndns-ldap config

--- bind-dyndb-ldap/build-bind-ldap.sh
+++ bind-dyndb-ldap/build-bind-ldap.sh
@@ -29,12 +29,12 @@ done
 service bind9 stop
 
 cat > /etc/bind/named.conf.local << EOF
-dynamic-db "bind9-dyndb" {
-    Library "ldap.so";
-    arg "uri ldapi:///";
-    arg "base ou=dns,dc=example,dc=net";
-    arg "auth_method sasl";
-    arg "sasl_mech EXTERNAL";
+dyndb bind9-dyndb "/usr/lib/bind/ldap.so" {
+    server_id "";
+    uri "ldapi:///";
+    base "ou=dns,dc=example,dc=net";
+    auth_method "sasl";
+    sasl_mech "EXTERNAL";
 };
 EOF
 
@@ -44,3 +44,15 @@ nameserver 127.0.0.1
 EOF
 
 service bind9 start
+// This is the primary configuration file for the BIND DNS server named.
+//
+// Please read /usr/share/doc/bind9/README.Debian.gz for information on the 
+// structure of BIND configuration files in Debian, *BEFORE* you customize 
+// this configuration file.
+//
+// If you are just adding zones, please do that in /etc/bind/named.conf.local
+
+include "/etc/bind/named.conf.options";
+include "/etc/bind/named.conf.local";
+include "/etc/bind/named.conf.default-zones";
+
